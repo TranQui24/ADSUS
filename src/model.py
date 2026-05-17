ebee431a63ed831fe954490ce6f606474ffb80d3
TranQui24 <tranqui240703@gmail.com>
2026-05-17 15:28:38 +0700

    Change model resnet 50 to YOLO

--- src/model.py
+++ src/model.py
@@ -1,36 +1,24 @@
 import torch
-import torch.nn as nn
-import torchvision.models as models
+from ultralytics import YOLO
 
-class HybridResNet50(nn.Module):
-    def __init__(self, in_channels=5, num_classes=1):
-        super(HybridResNet50, self).__init__()
-        
-        self.resnet = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        
-        
-        old_weight = self.resnet.conv1.weight.data
-        
-      
-        self.resnet.conv1 = nn.Conv2d(
-            in_channels=in_channels, 
-            out_channels=64, 
-            kernel_size=7, 
-            stride=2, 
-            padding=3, 
-            bias=False
+def get_hybrid_yolo(model_name="yolov8n.pt", in_channels=5, num_classes=1):
+    model = YOLO(model_name)
+    
+    if in_channels != 3:
+        old_conv = model.model.model[0].conv
+        new_conv = torch.nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=old_conv.out_channels,
+            kernel_size=old_conv.kernel_size,
+            stride=old_conv.stride,
+            padding=old_conv.padding,
+            bias=old_conv.bias is not None
         )
-        
-      
-        self.resnet.conv1.weight.data[:, :3] = old_weight
-        nn.init.kaiming_normal_(self.resnet.conv1.weight.data[:, 3:], mode='fan_out', nonlinearity='relu')
-        
-      
-        self.resnet.fc = nn.Linear(2048, num_classes)
-        self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x):
-
-        x = self.resnet(x)
-        x = self.sigmoid(x) 
-        return x
+        with torch.no_grad():
+            new_conv.weight[:, :3] = old_conv.weight
+            torch.nn.init.kaiming_normal_(new_conv.weight[:, 3:], mode='fan_out', nonlinearity='relu')
+            
+        model.model.model[0].conv = new_conv
+        
+    return model
